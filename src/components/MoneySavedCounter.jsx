c2d81adf4616de18d33403b17cf8370f2130074b
c-mccormick007 <c.mccormick1295@gmail.com>
2025-03-16 14:03:27 -0500

    make a cool box for my $saved

--- src/components/MoneySavedCounter.jsx
+++ src/components/MoneySavedCounter.jsx
@@ -25,8 +25,10 @@ const MoneySavedCounter =({ target = 10000, duration = 2000 }) => {
     });
 
     return (
-        <div className="text-4xl font-bold, text-green-400 text-center p-4">
-                {formatted} Saved
+        <div className="bg-gradient-to-br from-neutral-800 to-neutral-900 border border-green-500 rounded-2xl shadow-[0_10px_25px_rgba(0,255,100,0.2)] p-6 max-w-md mx-auto">
+            <div className="text-4xl font-bold, text-green-400 text-center p-4">
+                    {formatted} Saved
+            </div>
         </div>
     );
 };
